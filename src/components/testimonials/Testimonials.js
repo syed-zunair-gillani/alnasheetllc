5117f801f86d0032bb5e53c7b61274056eeef40e
syed zunair gillani <zunairgillani54@gmail.com>
2023-11-03 13:04:34 +0500

    autoplay

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -31,7 +31,7 @@ const Testimonials = () => {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 3000,
     dots: true,
     centerMode: true,
